f9d4ee6c5c9901d0ffce0a3b6f520e4a66ad71b1
Guirec Corbel <guirec.corbel@gmail.com>
2014-03-13 13:14:03 -0400

    require the right file

--- lib/activeform-rails.rb
+++ lib/activeform-rails.rb
@@ -5,5 +5,5 @@ end
 require 'active_support/core_ext/module/delegation'
 require 'active_record'
 require 'activeform-rails/form'
-require 'activeform-rails/mock_model'
+require 'activeform-rails/unpersistent_model'
 require 'activeform-rails/validate_uniqueness'
